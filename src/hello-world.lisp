5a8e87e70bd0166917a18b955f7545527296e2ba
Mike Travers <mt@alum.mit.edu>
2012-03-05 11:27:42 -0800

    add a static file

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -6,7 +6,15 @@
 			 (with-http-body (req ent)
 			   (html
 			    (:h1 "Hello World")
-			    (:princ "Congratulations, you are running Lisp on Heroku"))))))
+			    (:princ "Congratulations, you are running Lisp on Heroku")
+			    :p
+			    ((:img :src "lisp-glossy.jpg"))
+			    )))))
+
+(publish-directory
+ :prefix "/"
+ :destination (make-pathname :directory (append *build-dir* (append '("public")))))
+		   
 
 
 

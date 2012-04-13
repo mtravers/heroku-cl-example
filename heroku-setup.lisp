6a8da1218f3adba3b65f5795e2ab6a1672ca09bc
Jason Aeschliman <j.aeschliman@gmail.com>
2012-04-12 20:19:32 -0400

    added postgres support and corrected public directory

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -2,6 +2,14 @@
 
 (print ">>> Building system....")
 
+
+;;load postgres
+(ql:quickload :clsql)
+(push (truename (make-pathname :directory (pathname-directory (asdf:system-definition-pathname :clsql))))
+      asdf:*central-registry*)
+(ql:quickload :clsql-postgresql)
+
+
 (load (make-pathname :directory *build-dir* :defaults "example.asd"))
 
 (ql:quickload :example)
